42567e61892c8e933c5973c00b21d8e59c1e4223
hzchin <chinhanzhong@gmail.com>
2020-04-16 13:39:24 +0800

    update basic auth

--- KeyModel.js
+++ KeyModel.js
@@ -16,22 +16,20 @@ var key = {
 KeysSchema.statics.authenticate = function (clientid, cSecret, callback) {
   // Keys.findOne({ _id: clientid })
   //   .exec(function (err, key) {
-
-      if (err) {
-        return callback(err)
-      } else if (!key) {
-        //console.log("testing")
-        var err = new Error('Client not found.');
-        err.status = 401;
-        return callback("Client not Found", null);
-      }
-      if(cSecret == key.clientSecret) {
+  if(clientid == key.clientid){
+    if(cSecret == key.clientSecret) {
         //console.log("matched")
         return callback(null, key);
       } else {
         //console.log("failed")
         return callback("Invalid Client Secret", null );
       }
+  } else {
+    //console.log("testing")
+    var err = new Error('Client not found.');
+    err.status = 401;
+    return callback("Client not Found", null);
+  }
     // });
 }
 
